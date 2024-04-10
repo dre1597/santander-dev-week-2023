d48ac905ecc82d8714194b2964d146623b3d54ef
André Pereira <andrepereira97@yahoo.com>
2024-04-10 12:51:20 -0300

    :tada: init project

---
+++ src/main/java/org/example/devweek/DevWeekApplication.java
@@ -0,0 +1,13 @@
+package org.example.devweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeekApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DevWeekApplication.class, args);
+  }
+
+}
